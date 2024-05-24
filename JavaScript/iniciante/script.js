6240df49658c7c966daa92cd4e2ba62c8e9e3c28
carvalhoandre <andre_carvalho0@live.com>
2024-05-24 08:40:56 -0300

    feat: class 02 data types

---
+++ JavaScript/iniciante/script.js
@@ -0,0 +1,20 @@
+/* aula 01 - exercicios */
+var name = "André";
+var age = 25;
+var food;
+food = "Lasanha";
+var one;
+var two;
+var three;
+var four;
+var five;
+
+/* aula 02: tipos de dados - exercicios */
+var exec = "";
+var noNumber = "02";
+var age = 25;
+var name = "André";
+var lastName = "Carvalho";
+var fullName = name + lastName;
+var time = "It's time";
+console.log(typeof name);
