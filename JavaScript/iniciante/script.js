e166587b612552e555de421aa432e11012f2bf71
carvalhoandre <andre_carvalho0@live.com>
2024-05-25 18:58:59 -0300

    feat: 5 and 6

--- JavaScript/iniciante/script.js
+++ JavaScript/iniciante/script.js
@@ -35,3 +35,65 @@ var unidade = "kg";
 var peso = numero + unidade;
 var pesoPorDois = parseInt(numero) / 2 + unidade;
 console.log(pesoPorDois);
+
+/* aula 05 - 06: boolean e condições - exercicios */
+// Verifique se a sua idade é maior do que a de algum parente
+var myAge = 25;
+var motherAge = 60;
+
+// Dependendo do resultado coloque no console 'É maior', 'É igual' ou 'É menor'
+if (myAge > motherAge) {
+  console.log("É maior");
+} else if (myAge === motherAge) {
+  console.log("É igual");
+} else {
+  console.log("É menor");
+}
+
+// Qual valor é retornado na seguinte expressão?
+var expressao = 5 - 2 && 5 - " " && 5 - 2;
+console.log("expressao", expressao);
+
+// Verifique se as seguintes variáveis são Truthy ou Falsy
+var nome = "Andre";
+var idade = 28;
+var possuiDoutorado = false;
+var empregoFuturo;
+var dinheiroNaConta = 0;
+
+console.log(
+  "nome",
+  !!nome,
+  "idade",
+  !!idade,
+  "possuiDoutorado",
+  !!possuiDoutorado,
+  "empregoFuturo",
+  !!empregoFuturo,
+  "dinheiroNaConta",
+  !!dinheiroNaConta
+);
+
+// Compare o total de habitantes do Brasil com China (valor em milhões)
+var brasil = 207;
+var china = 1340;
+
+if (brasil > china) {
+  console.log("brasil");
+} else {
+  console.log("china");
+}
+
+// O que irá aparecer no console?
+if ("Gato" === "gato" && 5 > 2) {
+  console.log("Verdadeiro");
+} else {
+  console.log("Falso");
+}
+
+// O que irá aparecer no console?
+if ("Gato" === "gato" || 5 > 2) {
+  console.log("Gato" && "Cão");
+} else {
+  console.log("Falso");
+}
