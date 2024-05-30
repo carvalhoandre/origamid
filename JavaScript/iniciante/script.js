45d2c7eeca340b2e58b75c4f33c35b48db15294b
carvalhoandre <andre_carvalho0@live.com>
2024-05-30 18:15:22 -0300

    feat: 13 - 14: Arrays e Loops

--- JavaScript/iniciante/script.js
+++ JavaScript/iniciante/script.js
@@ -253,3 +253,32 @@ var button = document.querySelector(".button");
 
 //   e.clearSelection();
 // });
+
+/* aulas 13 - 14: Arrays e Loops - exercicios */
+title("tudo é objeto");
+
+// Crie uma array com os anos que o Brasil ganhou a copa
+// 1959, 1962, 1970, 1994, 2002
+var array = [1959, 1962, 1970, 1994, 2002];
+
+// Interaja com a array utilizando um loop, para mostrar
+// no console a seguinte mensagem, `O brasil ganhou a copa de ${ano}`
+for (year = 0; year < array.length; year++) {
+  console.log(`O brasil ganhou a copa de ${array[year]}`);
+}
+
+// Interaja com um loop nas frutas abaixo e pare ao chegar em Pera
+var frutas = ["Banana", "Maçã", "Pera", "Uva", "Melância"];
+
+for (i = 0; i < frutas.length; i++) {
+  if (frutas[i] === "Pera") {
+    break;
+  }
+
+  console.log(frutas[i]);
+}
+
+// Coloque a última fruta da array acima em uma variável,
+// sem remover a mesma da array.
+var lastFruit = frutas[frutas.length - 1];
+console.log("a última fruta:", lastFruit);
