16e8430f72586a2f07e866d8908058135db226a1
carvalhoandre <andre_carvalho0@live.com>
2024-06-14 08:47:41 -0300

    0312 Animação ao Scroll

--- JavaScript/animaisFantasticos/script.js
+++ JavaScript/animaisFantasticos/script.js
@@ -67,6 +67,32 @@ function initScrollSuave() {
   });
 }
 
+const sections = document.querySelectorAll(".js-scroll");
+
+function animationOnScroll() {
+  if (sections.length <= 0) return;
+
+  const windowRef = window.innerHeight * 0.6;
+
+  function animationScroll() {
+    sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = sectionTop - windowRef < 0;
+
+      if (isSectionVisible) {
+        section.classList.add("ativo");
+      } else {
+        section.classList.remove("ativo");
+      }
+    });
+  }
+
+  animationScroll();
+
+  window.addEventListener("scroll", animationScroll);
+}
+
 initTabNav();
 initAccordion();
 initScrollSuave();
+animationOnScroll();
