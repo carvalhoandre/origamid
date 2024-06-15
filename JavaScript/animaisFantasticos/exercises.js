8e8e88e035d092a17098572780e5d169e76970c1
carvalhoandre <andre_carvalho0@live.com>
2024-06-15 11:06:19 -0300

    0401 Constructor Function 2

--- JavaScript/animaisFantasticos/exercises.js
+++ JavaScript/animaisFantasticos/exercises.js
@@ -173,3 +173,36 @@ const dd = dt.nextElementSibling;
 // Substitua o conteúdo html de .faq pelo de .animais
 const animals = document.querySelector(".animais");
 fac.innerHTML = animals;
+
+// Transforme o objeto abaixo em uma Constructor Function
+function pessoa(nome, idade) {
+  (this.nome = nome),
+    (this.idade = idade),
+    (this.andar = function () {
+      console.log(this.nome + " andou");
+    });
+}
+
+// Crie 3 pessoas, João - 20 anos,
+// Maria - 25 anos, Bruno - 15 anos
+const joao = new pessoa("João", 20);
+const bruno = new pessoa("Bruno", 15);
+const maria = new pessoa("Maria", 25);
+
+// Crie uma Constructor Function (Dom) para manipulação
+// de listas de elementos do dom. Deve conter as seguintes
+// propriedades e métodos:
+//
+// elements, retorna NodeList com os elementos selecionados
+// addClass(classe), adiciona a classe a todos os elementos
+// removeClass(classe), remove a classe a todos os elementos
+function Dom(selector) {
+  this.element = document.querySelector(selector);
+
+  this.addClass = function (classAdd) {
+    this.element.classList.add(classAdd || "ativo");
+  };
+  this.removeClass = function (classAdd) {
+    this.element.classList.remove(classAdd || "ativo");
+  };
+}
