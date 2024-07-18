941e92b59cacc3ff01deff98c24ad9f3adab4b11
carvalhoandre <andre_carvalho0@live.com>
2024-07-18 15:05:52 -0300

    feat: 0507 Clickoutside

--- JavaScript/animaisFantasticos/modules/dropdownMenu.js
+++ JavaScript/animaisFantasticos/modules/dropdownMenu.js
@@ -1,15 +1,21 @@
+import outsideClick from "./outsideClick.js";
+
 export default function initDropdownMenu() {
   const dropDownMenus = document.querySelectorAll("[data-dropdown]");
 
   const events = ["click", "touchstart"];
 
+  function handleClick(event) {
+    event.preventDefault();
+
+    this.classList.toggle("active");
+
+    outsideClick(this, events, () => {
+      this.classList.remove("active");
+    });
+  }
+
   if (dropDownMenus.length > 0) {
-    function handleClick(event) {
-      event.preventDefault();
-
-      this.classList.toogle("active");
-    }
-
     dropDownMenus.forEach((menu) => {
       events.forEach((userEvent) => {
         menu.addEventListener(userEvent, handleClick);
