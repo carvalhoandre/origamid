3f0a98e387a1fec6f6160ac6651e24e684bd45b6
carvalhoandre <andre_carvalho0@live.com>
2024-08-10 18:36:34 -0300

    0608 Animais Fetch

--- JavaScript/fantasticAnimals/script.js
+++ JavaScript/fantasticAnimals/script.js
@@ -1,21 +1,21 @@
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
-import initNumbers from "./modules/numbers.js";
 import initTabNav from "./modules/initTabNav.js";
 import initMobileMenu from "./modules/mobileMenu.js";
 import openingHours from "./modules/openingHours.js";
 import initAccordion from "./modules/initiAccordion.js";
 import initScrollSuave from "./modules/scrollSuave.js";
 import initDropdownMenu from "./modules/dropdownMenu.js";
+import initFetchAnimals from "./modules/fetchAnimals.js";
 import animationOnScroll from "./modules/animationOnScroll.js";
 
 initModal();
 initTabNav();
 initTooltip();
-initNumbers();
 openingHours();
 initAccordion();
 initMobileMenu();
 initScrollSuave();
 initDropdownMenu();
+initFetchAnimals();
 animationOnScroll();
