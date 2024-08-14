91cfeac840d296920cd829589315d4248da2b185
carvalhoandre <andre_carvalho0@live.com>
2024-08-14 11:14:35 -0300

    feat: 0805 Rest e Spread

--- JavaScript/exercicies/script.js
+++ JavaScript/exercicies/script.js
@@ -83,3 +83,22 @@ const cachorro = {
 };
 
 const { cor: bobCor } = cachorro;
+
+// Reescreva a função utilizando
+// valores iniciais de parâmetros com ES6
+function createButton(background = "blue", color = "red") {
+  const buttonElement = document.createElement("button");
+
+  buttonElement.style.background = background;
+  buttonElement.style.color = color;
+
+  return buttonElement;
+}
+
+const redButton = createButton("#FFF", "#000");
+
+// Utilize o método push para inserir as frutas ao final de comidas.
+const frutas = ["Banana", "Uva", "Morango"];
+const comidas = ["Pizza", "Batata"];
+
+comidas.push(frutas);
