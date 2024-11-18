4234d9c4a87f2422bdded9160d58a2e797d45e40
carvalhoandre <andre_carvalho0@live.com>
2024-11-18 18:33:25 -0300

    feat: 0805 Header

--- react/ac-dogs/src/components/Header/index.jsx
+++ react/ac-dogs/src/components/Header/index.jsx
@@ -1,14 +1,21 @@
 import { Link } from "react-router-dom";
 import styles from "./styles.module.css";
 
+import Dog from "../../assets/dogs.svg?react";
+
 const Header = () => {
   return (
-    <div className={styles.container}>
-      <nav className="container">
-        <Link to="/">Home</Link>
-        <Link to="/login">Login / Criar</Link>
+    <header className={styles.container}>
+      <nav className={`${styles.nav} "container"`}>
+        <Link className={styles.logo} to="/" aria-label="AC - Dogs - Home">
+          <Dog />
+        </Link>
+
+        <Link to="/login" className={styles.login}>
+          Login / Criar
+        </Link>
       </nav>
-    </div>
+    </header>
   );
 };
 
