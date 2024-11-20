4884bfb66cae7b021fbe38a502c2f2eff318bb05
carvalhoandre <andre_carvalho0@live.com>
2024-11-20 19:22:07 -0300

    feat: 0813 Login Background

--- react/ac-dogs/src/routes/login/index.jsx
+++ react/ac-dogs/src/routes/login/index.jsx
@@ -8,18 +8,24 @@ import { Register } from "../../pages/register";
 import { PasswordReset } from "../../pages/passwordReset";
 import { PasswordRecovery } from "../../pages/passwordRecovery";
 
+import styles from "./styles.module.css";
+
 const LoginRoutes = () => {
   const { login } = React.useContext(UserContext);
 
   if (login === true) return <Navigate to="/conta" />;
 
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="register" element={<Register />} />
-      <Route path="password-reset" element={<PasswordReset />} />
-      <Route path="password-recovery" element={<PasswordRecovery />} />
-    </Routes>
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="password-reset" element={<PasswordReset />} />
+          <Route path="password-recovery" element={<PasswordRecovery />} />
+        </Routes>
+      </div>
+    </section>
   );
 };
 
