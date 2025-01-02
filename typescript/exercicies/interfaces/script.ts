958fcdc8c1ea766c7f17def4797098264fef1862
carvalhoandre <andre_carvalho0@live.com>
2025-01-02 14:51:15 -0300

    feat: 0302 Interfaces do DOM

---
+++ typescript/exercicies/interfaces/script.ts
@@ -0,0 +1,12 @@
+const links = document.querySelectorAll(".link");
+
+function activeElement(element: HTMLElement) {
+  element.style.color = "red";
+  element.style.border = "2px solid red";
+}
+
+links.forEach((link) => {
+  if (link instanceof HTMLElement) {
+    activeElement(link);
+  }
+});
