66a29aec2b334a644f58215496c344a2c357ddfe
carvalhoandre <andre_carvalho0@live.com>
2025-01-08 15:04:33 -0300

    feat: 0402 Tuples

---
+++ typescript/exercicies/tuples/index.ts
@@ -0,0 +1,31 @@
+// 1 - Faça um fetch das vendas: https://api.origamid.dev/json/vendas.json
+// 2 - Defina o tipo/interface de cada venda (tuple)
+// 3 - Some o total das vendas e mostre na tela
+
+interface ProductDetails {
+  marca: string;
+  cor: string;
+}
+
+type Sale = [string, number, string, ProductDetails];
+
+function handleData(sales: Array<Sale>) {
+  const sun = sales.reduce((acc, sale) => {
+    const currentValue = sale[1];
+
+    if (!!currentValue) return acc + currentValue;
+
+    return acc;
+  }, 0);
+
+  document.body.innerHTML += `<p>Soma da venda: R$ ${sun}`;
+}
+
+async function fetchSaleData(): Promise<void> {
+  const response = await fetch("https://api.origamid.dev/json/vendas.json");
+  const data: Array<Sale> = await response.json();
+
+  handleData(data);
+}
+
+fetchData();
