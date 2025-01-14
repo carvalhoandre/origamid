c6f95723c9d9ca21251ef9902d8f6cb5150198d7
carvalhoandre <andre_carvalho0@live.com>
2025-01-14 11:27:28 -0300

    feat: 0609 Preencher Lista

--- typescript/data/src/script.ts
+++ typescript/data/src/script.ts
@@ -1,5 +1,5 @@
 import Statistics from "./types/Statistics";
-import { Transaction, TransactionApi } from "./types/types";
+import { CountList, Transaction, TransactionApi } from "./types/types";
 
 import fetchData from "./fetchData";
 
@@ -23,8 +23,25 @@ function fillTable(items: Array<Transaction>): void {
   });
 }
 
+function fillList(list: CountList, countainerId: string): void {
+  console.log(list, countainerId);
+  const countainerElement = document.getElementById(countainerId);
+
+  console.log(countainerElement);
+  if (countainerElement) {
+    Object.keys(list).forEach((key) => {
+      countainerElement.innerHTML += `
+        <p> ${key}: ${list[key]}</p>
+      `;
+    });
+  }
+}
+
 function fillInStatistics(items: Array<Transaction>): void {
   const statistics = new Statistics(items);
+
+  fillList(statistics.payment, "payment");
+  fillList(statistics.status, "status");
 
   const sumElement = document.querySelector<HTMLElement>("#sum span");
 
