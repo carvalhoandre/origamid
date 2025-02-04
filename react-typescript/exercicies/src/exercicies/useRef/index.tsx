c08b1ec32677c713f7615a22ecfdc4d5cd5ece8d
carvalhoandre <andre_carvalho0@live.com>
2025-02-04 20:31:32 -0300

    fix: 0303 useRef TSX 2

--- react-typescript/exercicies/src/exercicies/useRef/index.tsx
+++ react-typescript/exercicies/src/exercicies/useRef/index.tsx
@@ -7,49 +7,60 @@ import "./styles.css";
 const PlayVideo = () => {
   const videoRef = React.useRef<HTMLVideoElement>(null);
 
-  const [isVideoPaused, setIsVideoPaused] = React.useState<boolean>(
-    videoRef.current?.paused || true
-  );
-
-  const onPlayOrPause = () => {
-    if (!videoRef.current) return;
-
-    setIsVideoPaused(!videoRef.current.paused);
-    return isVideoPaused ? videoRef.current.play() : videoRef.current.pause();
-  };
+  const [playing, setPlaying] = React.useState(false);
 
   const advanceVideo = () => {
     if (!videoRef.current) return;
 
-    videoRef.current.currentTime = videoRef.current.duration + 2;
+    videoRef.current.currentTime += 2;
   };
 
-  const updatePlaybackRate = (videoSpeed: number) => {
+  const changePlaybackRate = (videoSpeed: number) => {
     if (!videoRef.current) return;
 
     videoRef.current.playbackRate = videoSpeed;
   };
 
-  const updateMute = () => {
+  const changeMute = () => {
     if (!videoRef.current) return;
 
     videoRef.current.muted = !videoRef.current.muted;
   };
 
+  const pictureInPicture = async () => {
+    if (!videoRef.current) return;
+
+    if (document.pictureInPictureElement) {
+      await document.exitPictureInPicture();
+    } else {
+      await videoRef.current.requestPictureInPicture();
+    }
+  };
+
   return (
     <div>
       <div className="players">
-        <button onClick={onPlayOrPause}>
-          {isVideoPaused ? "Play" : "Pause"}
-        </button>
+        {playing ? (
+          <button onClick={() => videoRef.current?.pause()}>Pause</button>
+        ) : (
+          <button onClick={() => videoRef.current?.play()}>Play</button>
+        )}
         <button onClick={advanceVideo}>+ 2s</button>
-        <button onClick={() => updatePlaybackRate(1)}>1x</button>
-        <button onClick={() => updatePlaybackRate(2)}>2x</button>
-        <button>PiP</button>
-        <button onClick={updateMute}>M</button>
+        <button onClick={() => changePlaybackRate(1)}>1x</button>
+        <button onClick={() => changePlaybackRate(2)}>2x</button>
+        <button onClick={pictureInPicture}>PiP</button>
+        <button onClick={changeMute}>M</button>
       </div>
 
-      <video src={Video} ref={videoRef} autoPlay={false} loop={true}></video>
+      <video
+        loop
+        controls
+        src={Video}
+        ref={videoRef}
+        autoPlay={false}
+        onPlay={() => setPlaying(true)}
+        onPause={() => setPlaying(false)}
+      ></video>
     </div>
   );
 };
