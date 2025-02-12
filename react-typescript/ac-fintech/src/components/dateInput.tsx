bf13fdf2c0749d7e9e09e758c93a5d2927fd3b26
André Leite Carvalho <66704150+carvalhoandre@users.noreply.github.com>
2025-02-12 11:57:44 -0300

    feat: 0406 CSSProperties

--- react-typescript/ac-fintech/src/components/dateInput.tsx
+++ react-typescript/ac-fintech/src/components/dateInput.tsx
@@ -4,11 +4,34 @@ type DateInput = React.ComponentProps<"input"> & {
   label: string;
 };
 
+const generalStyles: React.CSSProperties = {
+  marginBottom: "var(--gap-s)",
+  fontWeight: "600",
+  fontSize: "1rem",
+  color: "var(--color-2)",
+  padding: "var(--gap-s)",
+  borderRadius: "var(--gap)",
+  background: "var(--color-4)",
+};
+
+const labelStyle: React.CSSProperties = {
+  display: "block",
+  ...generalStyles,
+};
+
+const inputStyle: React.CSSProperties = {
+  border: "none",
+  fontFamily: "monospace",
+  ...generalStyles,
+};
+
 const DateInput = ({ label, ...props }: DateInput) => {
   return (
     <>
-      <label htmlFor={label}>{label}</label>
-      <input type="date" id={label} {...props} />
+      <label style={labelStyle} htmlFor={label}>
+        {label}
+      </label>
+      <input type="date" id={label} style={inputStyle} {...props} />
     </>
   );
 };
