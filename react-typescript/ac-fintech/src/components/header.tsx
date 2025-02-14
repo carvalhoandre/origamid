1fff4790043acb537c75bce654027cf069e4c204
André Leite Carvalho <66704150+carvalhoandre@users.noreply.github.com>
2025-02-14 16:18:59 -0300

    feat: 0411 Sidenav

--- react-typescript/ac-fintech/src/components/header.tsx
+++ react-typescript/ac-fintech/src/components/header.tsx
@@ -1,11 +1,17 @@
+import React from "react";
 import DateRange from "./dateRange";
 import Months from "./months";
 
 const Header = () => {
+  const [title] = React.useState("Resumo")
+
+
   return (
     <header className="mb">
-      <div className="mb">
+      <div className="daterange mb">
         <DateRange />
+
+        <h1 className="box bg-3">{title}</h1>
       </div>
 
       <Months />
--- react-typescript/ac-fintech/src/components/sadenav.tsx
+++ react-typescript/ac-fintech/src/components/sadenav.tsx
@@ -1,11 +1,59 @@
-import { UseData } from "../context/dataContext";
+import FintechSVG from "../assets/FintechSVG";
+
+import SummarySVG from "../assets/icons/resumo.svg";
+import SalesSVG from "../assets/icons/vendas.svg";
+import WebHooksSVG from "../assets/icons/webhooks.svg";
+import ContactsSVG from "../assets/icons/contato.svg";
+import SettingsSVG from "../assets/icons/configuracoes.svg";
+import ExitSVG from "../assets/icons/sair.svg";
 
 const Sadenav = () => {
-      const { data } = UseData();
-    
-      console.log(data);
+  return (
+    <nav className="sidenav box bg-3">
+      <FintechSVG title="Fintech logo" />
 
-    return (<></>)
+      <ul>
+        <li>
+          <span>
+            <img src={SummarySVG} alt="Resumo" />
+          </span>
+
+          <a href="">Resumo</a>
+        </li>
+        <li>
+          <span>
+            <img src={SalesSVG} alt="Resumo" />
+          </span>
+          <a href="">Vendas</a>
+        </li>
+
+        <li>
+          <span>
+            <img src={WebHooksSVG} alt="Resumo" />
+          </span>
+          <a href="">Webhooks</a>
+        </li>
+        <li>
+          <span>
+            <img src={SettingsSVG} alt="Resumo" />
+          </span>
+          <a href="">Configurações</a>
+        </li>
+        <li>
+          <span>
+            <img src={ContactsSVG} alt="Resumo" />
+          </span>
+          <a href="">Contato</a>
+        </li>
+        <li>
+          <span>
+            <img src={ExitSVG} alt="Resumo" />
+          </span>
+          <a href="">Sair</a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Sadenav;
