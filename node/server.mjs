d5b1c63dc87f63ef3e856855a7342f20f60094d9
André Leite Carvalho <andre_carvalho0@live.com>
2026-02-19 19:16:06 -0300

    feat: 0205 Custom Req e Res 2

--- node/server.mjs
+++ node/server.mjs
@@ -1,27 +1,29 @@
 import { createServer } from 'node:http';
+
 import { Router } from './routes.mjs';
 import { customRequest } from './custom-request.mjs';
+import { customResponse } from './custom-response.mjs';
 
 const router = new Router();
 
 router.get('/', (req, res) => {
-  res.end('Home');
+  res.status(200).end('Home');
 });
 router.get('/produto/notebook', (req, res) => {
-  res.end('Produtos - Notebbok');
+  res.status(200).end('Produtos - Notebbok');
 });
 router.post('/produto', (req, res) => {
-  res.end('Notebbok Post');
+  res.status(201).json({ name: 'Notebook', color: 'blue' });
 });
 
 const server = createServer(async (request, response) => {
   const req = await customRequest(request);
+  const res = customResponse(response);
 
   const handler = router.find(req.method, req.pathname);
   if (handler) return handler(req, res);
 
-  res.statusCode = 404;
-  res.end('Nao encontrado');
+  res.status(404).end('Nao encontrado');
 });
 
 server.listen(3000, () => {
