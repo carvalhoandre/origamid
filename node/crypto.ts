9ef8c610c8abef41b6c737762db389589e72aff6
André Leite Carvalho <andre_carvalho0@live.com>
2026-05-08 10:29:01 -0300

    feat: 0510 Password 3

--- node/crypto.ts
+++ node/crypto.ts
@@ -4,6 +4,7 @@ import {
   scrypt,
   randomBytes,
   createHmac,
+  timingSafeEqual,
 } from "node:crypto";
 import { promisify } from "node:util";
 
@@ -15,8 +16,6 @@ const scryptAsync: (
   options?: ScryptOptions,
 ) => Promise<Buffer> = promisify(scrypt);
 
-const salt = await randomBytesAsync(16);
-
 const SCRYPT_OPTIONS: ScryptOptions = {
   N: 2 ** 14,
   r: 8,
@@ -25,9 +24,46 @@ const SCRYPT_OPTIONS: ScryptOptions = {
 
 const PEPPER = "segredo_super_secreto";
 
-const password_hmac = createHmac("sha256", PEPPER).update("12345678").digest();
+async function hashPassword(password: string): Promise<string> {
+  const password_normalized = password.normalize("NFC");
+  const password_hmac = createHmac("sha256", PEPPER)
+    .update(password_normalized)
+    .digest();
 
-const dk = await scryptAsync(password_hmac, salt, 32, SCRYPT_OPTIONS);
-const passwordHash = `${salt.toString("hex")}$${dk.toString("hex")}`;
+  const salt = await randomBytesAsync(16);
 
-console.log(passwordHash);
+  const dk = await scryptAsync(password_hmac, salt, 32, SCRYPT_OPTIONS);
+  return `${salt.toString("hex")}$${dk.toString("hex")}`;
+}
+
+function parsePasswordHash(passwordHash: string) {
+  const [stored_salt_hex, stored_dk_hex] = passwordHash.split("$");
+
+  const stored_dk = Buffer.from(stored_dk_hex, "hex");
+  const stored_salt = Buffer.from(stored_salt_hex, "hex");
+
+  return { stored_salt, stored_dk };
+}
+
+async function verifyPassword(password: string, passwordHash: string) {
+  const { stored_salt, stored_dk } = parsePasswordHash(passwordHash);
+
+  const password_normalized = password.normalize("NFC");
+  const password_hmac = createHmac("sha256", PEPPER)
+    .update(password_normalized)
+    .digest();
+
+  const dk = await scryptAsync(password_hmac, stored_salt, 32, SCRYPT_OPTIONS);
+
+  if (dk.length !== stored_dk.length) return false;
+
+  return timingSafeEqual(dk, stored_dk);
+}
+
+const password = "12345678";
+const password_hash = await hashPassword(password);
+
+const isTrue = await verifyPassword(password, password_hash);
+const isFalse = await verifyPassword("wrong_password", password_hash);
+
+console.log({ isTrue, isFalse });
