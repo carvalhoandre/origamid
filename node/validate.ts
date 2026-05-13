9226b3ff794e197cf54ffe782b46118eb82a0fa3
André Leite Carvalho <andre_carvalho0@live.com>
2026-05-13 11:32:23 -0300

    feat: 0603 Formatar 1

--- node/validate.ts
+++ node/validate.ts
@@ -8,3 +8,19 @@ function number(x: unknown): number | undefined {
   return undefined;
 }
 
+function boolean(x: unknown): boolean | undefined {
+  if (x === true || x === "true" || x === 1 || x === "1" || x === "on")
+    return true;
+  if (x === false || x === "false" || x === 0 || x === "0" || x === "off")
+    return false;
+  return undefined;
+}
+
+function cpf(x: string): string | undefined {
+  return x.replace(/\D+/g, "");
+}
+
+function email(x: string): string | undefined {
+  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return re.test(x) ? x.toLocaleLowerCase() : undefined;
+}
