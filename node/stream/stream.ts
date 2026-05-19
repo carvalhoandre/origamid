1a2747ea7589934946a7bad7735f3c654a953467
André Leite Carvalho <andre_carvalho0@live.com>
2026-05-19 11:57:21 -0300

    feat: 0702 Stream 2

--- node/stream/stream.ts
+++ node/stream/stream.ts
@@ -1,28 +1,26 @@
 import { createReadStream, createWriteStream, writeFile } from "node:fs";
 import { pipeline } from "node:stream";
 
-const { readFile } = require("node:fs");
+// setInterval(() => {
+//   console.log("Aguardando arquivos...");
+//   for (let i = 0; i < 10; i++) {
+//     receiveFileStream(i);
+//   }
+// }, 3000);
 
-setInterval(() => {
-  console.log("Aguardando arquivos...");
-  for (let i = 0; i < 10; i++) {
-    receiveFileStream(i);
-  }
-}, 3000);
+// async function receiveFile(i: number) {
+   // const body = await readFile("./dados.json");
 
-async function receiveFile(i: number) {
-  const body = await readFile("./entrada.txt", "utf-8");
-
-  await writeFile(`./saida${i}.txt`, body, (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-  });
-}
+//   await writeFile(`./saida${i}.txt`, body, (err) => {
+//     if (err) {
+//       console.error(err);
+//       return;
+//     }
+//   });
+// }
 
 async function receiveFileStream(i: number) {
-    const readStream = createReadStream("./entrada.txt", "utf-8");
+    const readStream = createReadStream("./data.json");
     const writeStream = createWriteStream(`./saida${i}.txt`);
 
     pipeline(readStream, writeStream, (err) => {
@@ -33,3 +31,18 @@ async function receiveFileStream(i: number) {
         }
     });
 }
+
+async function readStream() {
+    const file = createReadStream("./data.json")
+    // const dada = await file.toArray();
+    const chunks = await file.toArray();
+
+    for await (const chunk of file) {
+       chunks.push(chunk);
+    }
+
+    const newData = Buffer.concat(chunks);
+    console.log(JSON.parse(newData.toString()).name);
+}
+
+readStream();
