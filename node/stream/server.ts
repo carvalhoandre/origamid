a6bad9812b4b31ca88e7f88767b84cf9195f7290
André Leite Carvalho <andre_carvalho0@live.com>
2026-05-20 11:01:31 -0300

    feat: 0702 Stream 4

--- node/stream/server.ts
+++ node/stream/server.ts
@@ -1,14 +1,24 @@
-import { createWriteStream } from "fs";
+import { createWriteStream, createReadStream } from "fs";
 import { createServer } from "http";
+import { pipeline } from "stream/promises";
 
 const log = createWriteStream("server.log", { flags: "a" });
 
-const server = createServer((req, res) => {
-    log.write(`${new Date().toISOString()} - ${req.method} ${req.url} ${req.socket.remoteAddress}\n`);
-    log.write('Antes do teste\n');
-    res.end("Olá, mundo!");
+const server = createServer(async (req, res) => {
+  try {
+    const data = createReadStream("../data.json");
+
+    log.write(
+      `${new Date().toISOString()} - ${req.method} ${req.url} ${req.socket.remoteAddress}\n`,
+    );
+    res.setHeader("Content-Type", "application/json");
+    await pipeline(data, res);
+  } catch (err) {
+    res.statusCode = 500;
+    res.end("Internal Server Error");
+  }
 });
 
 server.listen(3009).on("close", () => {
-    log.end();
+  log.end();
 });
