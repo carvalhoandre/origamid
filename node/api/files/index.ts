b5f5dd998593866349d3ac780aae632bacd064d6
André Leite Carvalho <andre_carvalho0@live.com>
2026-05-21 15:53:55 -0300

    feat: 0705 Upload Arquivos 1

--- node/api/files/index.ts
+++ node/api/files/index.ts
@@ -1,5 +1,11 @@
 import path from "node:path";
-import { createReadStream, createWriteStream, statSync } from "node:fs";
+import {
+  rm,
+  statSync,
+  createReadStream,
+  createWriteStream,
+  renameSync,
+} from "node:fs";
 import { pipeline } from "node:stream/promises";
 
 import { checkETag, mimeType } from "./utils.ts";
@@ -7,12 +13,17 @@ import { checkETag, mimeType } from "./utils.ts";
 import { Api } from "../../core/utils/abstract.ts";
 import { validate } from "../../core/utils/validate.ts";
 import { RouteError } from "../../core/utils/router-error.ts";
+import { randomUUID } from "node:crypto";
+
+const MAX_BYTES = 150 * 1024 * 1024; // 150MB
+
+const FILES_PATH = "./files";
 
 export class FilesApi extends Api {
   handlers = {
     sendFile: async (req, res) => {
       const name = validate.file(req.params.name);
-      const filePath = `./files/${name}`;
+      const filePath = `${FILES_PATH}/${name}`;
       const ext = path.extname(name);
       let stats;
 
@@ -47,12 +58,39 @@ export class FilesApi extends Api {
     },
 
     uploadFile: async (req, res) => {
-      const name = req.headers["x-filename"];
-      const writeStream = createWriteStream(`./files/${name}`);
-  
-      await pipeline(req, writeStream);
+      const contentLength = Number(req.headers["content-length"]);
+      if (!req.headers["x-filename"]) {
+        throw new RouteError(400, "Erro ao enviar arquivo");
+      }
 
-      res.end("Upload de arquivos não implementado");
+      if (req.headers["content-type"] !== "application/octet-stream") {
+        throw new RouteError(415, "Erro ao enviar arquivo");
+      }
+
+      if (!contentLength || isNaN(contentLength) || contentLength <= 0) {
+        throw new RouteError(400, "Erro ao enviar arquivo");
+      }
+
+      if (contentLength > MAX_BYTES) {
+        throw new RouteError(413, "Arquivo muito grande");
+      }
+
+      const name = validate.file(req.headers["x-filename"]);
+      const tempPath = `${FILES_PATH}/${randomUUID()}`;
+      const writeStream = createWriteStream(tempPath);
+      const writePath = `${FILES_PATH}/${name}`;
+
+      try {
+        await pipeline(req, writeStream);
+        renameSync(tempPath, writePath);
+
+        res.status(201).end("Arquivo enviado com sucesso");
+      } catch (err) {
+        console.error(err);
+        throw new RouteError(500, "Erro ao salvar arquivo");
+      } finally {
+        rm(tempPath, { force: true }, () => {});
+      }
     },
   } satisfies Api["handlers"];
 
