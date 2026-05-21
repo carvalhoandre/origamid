9ffbf420a146c8ca3992f86b4c6ea230bfe1c1b6
André Leite Carvalho <andre_carvalho0@live.com>
2026-05-21 15:17:47 -0300

    feat: 0704 Servir Arquivos 3

--- node/api/files/index.ts
+++ node/api/files/index.ts
@@ -1,14 +1,46 @@
-import { createReadStream } from "node:fs";
+import path from "node:path";
+import { createReadStream, statSync } from "node:fs";
 import { pipeline } from "node:stream/promises";
+
+import { checkETag, mimeType } from "./utils.ts";
 
 import { Api } from "../../core/utils/abstract.ts";
 import { validate } from "../../core/utils/validate.ts";
+import { RouteError } from "../../core/utils/router-error.ts";
 
 export class FilesApi extends Api {
   handlers = {
     sendFile: async (req, res) => {
       const name = validate.file(req.params.name);
       const filePath = `./files/${name}`;
+      const ext = path.extname(name);
+      let stats;
+
+      try {
+        stats = statSync(filePath);
+      } catch (err) {
+        throw new RouteError(404, "Arquivo não encontrado");
+      }
+      const etag = `W/${stats.size.toString(16)}-${Math.floor(stats.mtimeMs).toString(16)}`;
+
+      res.setHeader("ETag", etag);
+      res.setHeader("Content-Length", stats.size);
+      res.setHeader("X-Content-Type-Options", "nosniff");
+      res.setHeader("Last-Modified", stats.mtime.toUTCString());
+      res.setHeader("Cache-Control", "public, max-age=0, must-revalidate");
+      res.setHeader(
+        "Content-Type",
+        mimeType[ext] || "application/octet-stream",
+      );
+
+      if (checkETag(req.headers["if-none-match"], etag)) {
+        res.status(304);
+        res.end();
+        return;
+      }
+
+      res.status(200);
+
       const file = createReadStream(filePath);
 
       await pipeline(file, res);
