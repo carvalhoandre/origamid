5ecff6b0068d8f72f9a6ff95522622a8eceec9e8
André Leite Carvalho <andre_carvalho0@live.com>
2026-06-03 15:42:04 -0300

    feat: 0901 Docker 3

--- node/index.js
+++ node/index.js
@@ -2,5 +2,5 @@ import { createServer } from 'node:http';
 
 createServer((req, res) => {
   res.statusCode = 200;
-  res.end('funcionou agora!');
+  res.end(`Ambiente de ${process.env.NODE_ENV}`);
 }).listen(3000);
