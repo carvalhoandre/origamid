0d01af1bef5e8ff494cc25715b359d25bada22ca
André Leite Carvalho <andre_carvalho0@live.com>
2026-06-09 08:57:40 -0300

    feat: 0903 Caddy 4

--- node/index.js
+++ node/index.js
@@ -1,6 +1,17 @@
 import { createServer } from 'node:http';
 
 createServer((req, res) => {
-  res.statusCode = 200;
-  res.end(`Ambiente de ${process.env.NODE_ENV}`);
+  const serssion = false;
+  
+  if (serssion) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.setHeader('X-Accel-Redirect', req.url);
+    res.end(`<h1>Bem Vindo</h1>`);
+
+  } else {
+    res.writeHead(401, { 'Content-Type': 'text/html' });
+    res.end(`<h1>Não autorizado</h1>`);
+  }
+
+  res.end(``);
 }).listen(3000);
